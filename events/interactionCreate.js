4ae98f8e79fe80b48453b71566dde604658fd11e
Noy Bueno <noybueno90@gmail.com>
2021-09-29 01:38:18 +0300

    added check for id of the button clicked

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,24 +1,27 @@
 module.exports = async (client, interaction) => {
     if (!interaction.isButton()) return;
     
-    if(client.tickets[interaction.guildId][interaction.member.id])
+    if(interaction.customId == "create-ticket")
     {
-        interaction.member.send("כבר יש לך טיקט פתוח, שלח הודעה בצ'אט כאן והיא תעבור באופן אנונימי לצוות התומכים.")
-    } else {
-        var keys = Object.keys(client.tickets[interaction.guildId])
-        var id = keys.length == 0 ? 1 : client.tickets[interaction.guildId][keys[keys.length - 1]].id + 1
+        if(client.tickets[interaction.guildId][interaction.member.id])
+        {
+            interaction.member.send("כבר יש לך טיקט פתוח, שלח הודעה בצ'אט כאן והיא תעבור באופן אנונימי לצוות התומכים.")
+        } else {
+            var keys = Object.keys(client.tickets[interaction.guildId])
+            var id = keys.length == 0 ? 1 : client.tickets[interaction.guildId][keys[keys.length - 1]].id + 1
 
-        client.tickets[interaction.guildId][interaction.member.id] = {
-            id: id,
-            manager: null
+            client.tickets[interaction.guildId][interaction.member.id] = {
+                id: id,
+                manager: null
+            }
+
+            var tickets = interaction.guild.channels.cache.find(c => c.type == "GUILD_CATEGORY" && c.name == client.settings.tickets_category)
+            interaction.guild.channels.create(`צאט-${id}`, {
+                type: "text",
+                parent: tickets.id
+            })
+
+            interaction.member.send("היי, צוות התומכים קיבל את הודעתכם בהצלחה!\nכל הודעה שתשלח כאן תגיע באופן אנונימי לצוות התומכים.");
         }
-
-        var tickets = interaction.guild.channels.cache.find(c => c.type == "GUILD_CATEGORY" && c.name == client.settings.tickets_category)
-        interaction.guild.channels.create(`צאט-${id}`, {
-            type: "text",
-            parent: tickets.id
-        })
-
-        interaction.member.send("היי, צוות התומכים קיבל את הודעתכם בהצלחה!\nכל הודעה שתשלח כאן תגיע באופן אנונימי לצוות התומכים.");
     }
 }
